511c88b300ba9116399f0d455cc973bb3ed39526
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2020-04-17 11:32:26 +0900

    add fis project

--- angular-reddit/src/app/app.module.ts
+++ angular-reddit/src/app/app.module.ts
@@ -1,12 +1,15 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
